ab29aa69c5d9c5a893d77df40989ba9a5a0747a4
nankuF <fireloki87@gmail.com>
2022-07-21 13:19:48 +0600

    add scripts for compile to .exe

---
+++ get_skills.py
@@ -0,0 +1,52 @@
+"""
+Вариант скрипта для компиляции в .exe
+Использование на свой страх и риск, на ошибки и баги не проверялось.
+"""
+import time
+
+from main import run_skills_counter
+
+
+def main():
+    print('Чтобы прервать выполнение программы нажмите Ctrl+C')
+    print()
+    print("""
+       Пример запроса:
+       - Введите название вакансии: Программист Python
+       - Введите местоположение на русском языке (страну, регион или город): Краснодарский край
+
+       Ключевые навыки будут сохранены в папку "skills".
+       """)
+    while True:
+        while True:
+            vacancy = input('- Введите название вакансии: ')
+            if not vacancy.isalpha():
+                print('Название вакансии должно состоять только из букв.')
+                continue
+            break
+
+        while True:
+            location = input('- Введите местоположение на русском языке (страну, регион или город): ')
+            if not location.isalpha():
+                if '-' in location or ' ' in location:
+                    pass
+                else:
+                    print('Название местоположения может состоять только из букв, знака дефиса и пробела.')
+                    continue
+            break
+
+        try:
+            run_skills_counter(vacancy, location, period=30)
+            break
+        except:
+            print()
+            print('Скорей всего вы опечатались, попробуйте снова.')
+            print()
+            continue
+
+    print('Окно закроется автоматически через 3 секунды.')
+    time.sleep(3)
+
+
+if __name__ == '__main__':
+    main()
